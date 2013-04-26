4e4dff064ce9e432f93a7080da4b751975f8b608
Matthew Johnston <warmwaffles@gmail.com>
2013-04-26 15:09:37 -0500

    added initial record

---
+++ mattjohnston.co.lua
@@ -0,0 +1,11 @@
+-- File: mattjohnston.co.lua
+-- Zone: mattjohnson.co
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "mattjohnston.co"
+
+-- A records
+a(_a, "54.215.4.10")
+
+-- CNAME records
+cname("www", _a)
