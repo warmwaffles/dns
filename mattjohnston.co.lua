376f840325e6086d2457934e9d3108bdd0e9aa79
Matthew Johnston <warmwaffles@gmail.com>
2014-02-17 21:54:15 -0600

    added psyrendb shenanigans

--- mattjohnston.co.lua
+++ mattjohnston.co.lua
@@ -1,9 +1,8 @@
 -- File: mattjohnston.co.lua
--- Zone: mattjohnson.co
+-- Zone: mattjohnston.co
 -- SOA record is automatically generated
 -- Variable _a is replaced with zone name
 -- _a = "mattjohnston.co"
--- moved to bitbucket
 
 -- A records
 a(_a, "162.243.64.127")
---
+++ psyrendb.com.lua
@@ -0,0 +1,11 @@
+-- File: psyrendb.com.lua
+-- Zone: psyrendb.com
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "psyrendb.com"
+
+-- A records
+a("salt.psyrendb.com", "107.170.21.75")
+
+-- CNAME records
+-- cname("www", _a)
