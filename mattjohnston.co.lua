e54d563b80a8b4a313dcae7b6b5e6c5d97a598b8
Matthew Johnston <warmwaffles@gmail.com>
2014-05-14 23:44:40 -0500

    bump

--- mattjohnston.co.lua
+++ mattjohnston.co.lua
@@ -11,4 +11,4 @@ a(_a, "162.243.64.127")
 cname("www", _a)
 
 -- keybase.io Record For Proof
-txt('@', 'keybase-site-verification=m07gqHcdRfObfZznDiwTVsxyK-AybilYHtaZHwUFSKw')
+txt(_a, 'keybase-site-verification=m07gqHcdRfObfZznDiwTVsxyK-AybilYHtaZHwUFSKw')
