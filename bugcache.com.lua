06f2b895a6a1be4070448b44cac65f5198bfe18c
Matthew Johnston <warmwaffles@gmail.com>
2017-08-26 15:24:44 -0500

    Added bugcache.com initial cname stuff

---
+++ bugcache.com.lua
@@ -0,0 +1,8 @@
+-- File: bugcache.com.lua
+-- Zone: bugcache.com
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "bugcache.com"
+
+cname("", "bugcache.com.herokudns.com")
+cname("www", "bugcache.com.herokudns.com")
