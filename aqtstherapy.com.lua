5a7af4ea3964b450b72be16c3e503c1190c7b4fc
Matthew Johnston <warmwaffles@gmail.com>
2017-12-01 11:13:02 -0600

    Add aqtstherapy

---
+++ aqtstherapy.com.lua
@@ -0,0 +1,7 @@
+-- File: aqtstherapy.com.lua
+-- Zone: aqtstherapy.com
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "aqtstherapy.com"
+
+google_app(_a)
