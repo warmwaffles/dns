c60ed1614ded3174c58cb317015ecb158d6a057b
Matthew Johnston <warmwaffles@gmail.com>
2019-10-17 20:35:32 -0500

    added neuroperformancept.com

---
+++ neuroperformancept.com.lua
@@ -0,0 +1,9 @@
+-- File: neuroperformancept.com.lua
+-- Zone: neuroperformancept.com
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "neuroperformancept.com"
+
+google_app(_a)
+
+txt(_a, "google-site-verification=xJVsZ7Jp7L8OraK7pGHx9KGMZB_yBwx8zxYobddxq5I")
